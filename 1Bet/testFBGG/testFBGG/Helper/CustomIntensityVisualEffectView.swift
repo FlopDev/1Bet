3f49d6c7c90036e5d140e2ff78e0a051e7593fa0
Flop79 <peyronyflorian.pro@gmail.com>
2024-05-16 22:54:12 +0200

    add the CustomIntensityVisuallEffectiView

--- 1Bet/testFBGG/testFBGG/Helper/CustomIntensityVisualEffectView.swift
+++ 1Bet/testFBGG/testFBGG/Helper/CustomIntensityVisualEffectView.swift
@@ -2,22 +2,7 @@
 //  CustomIntensityVisualEffectView.swift
 //  testFBGG
 //
-//  Created by Florian Peyrony on 17/04/2023.
+//  Created by Florian Peyrony on 16/05/2024.
 //
 
 import Foundation
-import UIKit
-
-class CustomIntensityVisualEffectView: UIVisualEffectView {
-    var intensity: CGFloat
-    
-    init(effect: UIVisualEffect, intensity: CGFloat) {
-        self.intensity = intensity
-        super.init(effect: effect)
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-}
-
